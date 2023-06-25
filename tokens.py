5b1d32015ce8990cddfff14535f7017dfd06165f
who <91316548+SalehB1@users.noreply.github.com>
2023-06-25 23:00:09 +0330

    [Add] token class

---
+++ tokens.py
@@ -0,0 +1,4 @@
+class Tokens:
+    guest_token = None
+    csrf_token = None
+    Authorization = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
