b4aa6a7f255f2efbd7c7685d812a882a7cc0d1c5
who <91316548+SalehB1@users.noreply.github.com>
2023-06-26 17:52:28 +0330

    [modify] remove fetch instruction

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-
 import requests
+import json
 from tokens import Tokens
+from guestToken import currentGuestToken, newGuestToken
+from constants import AUTHORIZATION, apiBase
+from defaultFetch import defaultFetch
+from typing import List, Dict, Any
 
 
 def main():
@@ -11,105 +14,49 @@ def main():
     print(r.text)
 
 
-class Twitter:
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/',
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'X-Twitter-Active-User': 'yes',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Referer': 'https://twitter.com/',
-            'Connection': 'keep-alive',
-        })
-        # self.session.headers.update({'x-csrf-token': Tokens.csrf_token})
-        self.session.headers.update({'authorization': Tokens.Authorization})
-        self.session.headers.update({'x-guest-token': self.__get_guest_token()})
-        self.session.headers.update({
-            'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'})
-        self.login_url_1 = 'https://api.twitter.com/1.1/onboarding/task.json?flow_name=login'
+class Twitter():
 
-    def __get_guest_token(self):
-        result = self.session.post("https://api.twitter.com/1.1/guest/activate.json", data='')
-        print('X-Guest-Token: ' + result.json()['guest_token'])
-        return result.json()['guest_token']
+    async def idToUnparsedTweets(tweetID: str, cursor: str,
+                                 includeRecommendedTweets: bool = False,
+                                 fetchFn: callable = defaultFetch) -> List[Dict[str, Any]]:
+        variables = {
+            "focalTweetId": tweetID,
+            "with_rux_injections": includeRecommendedTweets,
+            "includePromotedContent": False,
+            "withCommunity": True,
+            "withQuickPromoteEligibilityTweetFields": False,
+            "withBirdwatchNotes": False,
+            "withSuperFollowsUserFields": False,
+            "withDownvotePerspective": False,
+            "withReactionsMetadata": False,
+            "withReactionsPerspective": False,
+            "withSuperFollowsTweetFields": False,
+            "withVoice": False,
+            "withV2Timeline": True,
+            "__fs_responsive_web_like_by_author_enabled": False,
+            "__fs_dont_mention_me_view_api_enabled": False,
+            "__fs_interactive_text_enabled": True,
+            "__fs_responsive_web_uc_gql_enabled": False,
+            "__fs_responsive_web_edit_tweet_api_enabled": False
+        }
+        if cursor != "":
+            variables["cursor"] = cursor
 
-    def __login(self):
-        payload = json.dumps({
-            "input_flow_data": {
-                "flow_context": {
-                    "debug_overrides": {},
-                    "start_location": {
-                        "location": "settings"
-                    }
-                }
-            },
-            "subtask_versions": {
-                "action_list": 2,
-                "alert_dialog": 1,
-                "app_download_cta": 1,
-                "check_logged_in_account": 1,
-                "choice_selection": 3,
-                "contacts_live_sync_permission_prompt": 0,
-                "cta": 7,
-                "email_verification": 2,
-                "end_flow": 1,
-                "enter_date": 1,
-                "enter_email": 2,
-                "enter_password": 5,
-                "enter_phone": 2,
-                "enter_recaptcha": 1,
-                "enter_text": 5,
-                "enter_username": 2,
-                "generic_urt": 3,
-                "in_app_notification": 1,
-                "interest_picker": 3,
-                "js_instrumentation": 1,
-                "menu_dialog": 1,
-                "notifications_permission_prompt": 2,
-                "open_account": 2,
-                "open_home_timeline": 1,
-                "open_link": 1,
-                "phone_verification": 4,
-                "privacy_options": 1,
-                "security_key": 3,
-                "select_avatar": 4,
-                "select_banner": 2,
-                "settings_list": 7,
-                "show_code": 1,
-                "sign_up": 2,
-                "sign_up_review": 4,
-                "tweet_selection_urt": 1,
-                "update_users": 1,
-                "upload_media": 1,
-                "user_recommendations_list": 4,
-                "user_recommendations_urt": 1,
-                "wait_spinner": 3,
-                "web_modal": 1
-            }
-        })
+        url = apiBase + "graphql/L1DeQfPt7n3LtTvrBqkJ2g/TweetDetail?variables=" + \
+              requests.utils.quote(json.dumps(variables))
 
-        def get_flow_token():
-            url = 'https://api.twitter.com/1.1/onboarding/task.json?flow_name=login'
-            result = self.session.post(url, data=payload)
-            if result.json()['status'] == 'success':
-                self.flow_token = result.json()['flow_token']
+        guestToken = currentGuestToken() or await newGuestToken(fetchFn)
+        obj = await fetchFn(url, "GET", AUTHORIZATION, guestToken)
+        if obj and obj.get("errors"):
+            if obj["errors"]["code"] in [215, 200]:
+                guestToken = await newGuestToken(fetchFn)
+                obj = await fetchFn(url, "GET", AUTHORIZATION, guestToken)
             else:
-                raise Exception('Failed to get flow token')
+                print(f"twitter get request error code: {obj['errors']['code']}")
 
-        def step2():
-            pass
-
-        def step3():
-            pass
-
-        def get_challenge():
-            result = self.session.get("https://api.twitter.com/1.1/auth/complete.json")
-            return result.json()['challenge']
+        obj = obj.get("data", {}).get("threaded_conversation_with_injections_v2", {}).get("instructions", [{}])[0]
+        tweets = obj.get("entries") or obj.get("moduleItems")
+        return tweets
 
 
 if __name__ == '__main__':
