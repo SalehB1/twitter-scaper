b26cff77dfe9cb02f12d10df6419dcec93686f02
who <91316548+SalehB1@users.noreply.github.com>
2023-06-26 19:18:20 +0330

    [modify] add api_base

--- tokens.py
+++ tokens.py
@@ -2,3 +2,4 @@ class Tokens:
     guest_token = None
     csrf_token = None
     Authorization = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
+    api_base = 'https://api.twitter.com/'
